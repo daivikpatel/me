f0e8e2290bb524b15c3f821c52e1ea4e08192a8a
daivikpatel <54406073+daivikpatel@users.noreply.github.com>
2021-06-28 20:32:16 -0400

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,32 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world,ground,ball;
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create();
+  world= engine.world;
+  var groundoptions={
+    isStatic:true
+  }
+  ground=Bodies.rectangle(200,390,200,20,groundoptions);
+  World.add(world,ground);
+var balloptions={
+  restitution:1.0
+}
+ball=Bodies.circle(200,100,20,balloptions);
+World.add(world,ball);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20);
+
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20,20);
+  drawSprites();
+}
